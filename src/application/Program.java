7c1de2756f1b64b577b8122114a1def011dffe65
Matheus Kieling <matheus.kieling@hotmail.com>
2023-03-30 10:33:40 -0300

    Implementacao piece, board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Position position = new Position(0,0);
-        System.out.println(position);
-
+        Board board = new Board(8, 8);
 
     }
 }
